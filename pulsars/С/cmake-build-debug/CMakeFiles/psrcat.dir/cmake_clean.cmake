661d8a783fe1c9038d71cb7cc7d2407e6ac2b4fb
dimicorn <zagorulia.ds@phystech.edu>
2021-10-21 02:30:36 +0300

    adding pulsar files

---
+++ pulsars/С/cmake-build-debug/CMakeFiles/psrcat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/psrcat.dir/main.cpp.obj"
+  "libpsrcat.dll.a"
+  "psrcat.exe"
+  "psrcat.exe.manifest"
+  "psrcat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/psrcat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ pulsars/С/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+class Pulsar{
+public:
+    explicit Pulsar(const std::vector<std::pair<std::string, std::string>>& v):length_(v.size()){
+        for (int i = 0; i < length_; ++i){
+            if (v[i].first == "PSRB"){
+                Name.first = "PSRB";
+                Name.second = v[i].second;
+            }
+            else if (v[i].first == "PSRJ"){
+                JName.first = "PSRJ";
+                JName.second = v[i].second;
+            }
+            else if (v[i].first == "RaJ"){
+                RaJ.first = "RaJ";
+                RaJ.second = v[i].second;
+            }
+        }
+    }
+    void GetName() const{
+        std::cout << Name.first << " " << Name.second << std::endl;
+    };
+    void GetJName() const{
+        std::cout << JName.first << " " << JName.second << std::endl;
+    }
+    void GetRaJ() const{
+        std::cout << RaJ.first << " " << RaJ.second << std::endl;
+    }
+private:
+    unsigned int length_;
+    std::pair<std::string, std::string> Name, JName, RaJ;
+};
+int main() {
+    std::string line;
+    std::ifstream myfile ("C:/Users/dmitr/Desktop/psrcat/psrcat.txt");
+    std::ofstream output ("C:/Users/dmitr/Desktop/psrcat/new_psrcat.txt");
+    if (myfile.is_open() and output.is_open()) {
+        while (getline (myfile, line)) {
+            std::stringstream input (line);
+            std::vector<std::pair<std::string, std::string>> v;
+            if (!input){
+                continue;
+            }
+            std::string first, second;
+            input >> first;
+            if (first[0] != '#' and first[0] != '@') {
+                input >> second;
+                std::pair<std::string, std::string> p(first, second);
+                output << first << ' ' << second << std::endl;
+                v.push_back(p);
+            }
+            if (first[0] == '@'){
+                Pulsar p(v);
+                output << '@' << std::endl;
+                v.clear();
+            }
+            // new pulsar,  continue
+        }
+        myfile.close();
+    }
+    else {
+        std::cout << "Unable to open file";
+    }
+
+    return 0;
+}
