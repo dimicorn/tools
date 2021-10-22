ae0bcdf3c15744ff1ff52e18fbe40ce8596aeefa
dimicorn <zagorulia.ds@phystech.edu>
2021-10-22 16:37:21 +0300

    bug fix

--- pulsars/Py/main.py
+++ pulsars/Py/main.py
@@ -10,10 +10,11 @@ def main():
     for i in range(len(data)):
         if data[0][i] != "@" and data[0][i] not in keys:
             keys.append(data[0][i])
-    print(keys)
+    # print(keys)
 
     for i in keys:
         temp[i] = "Nan"
+    # print(temp["P0"])
     for i in range(len(data)):
         if data[0][i] != "@":
             temp[data[0][i]] = data[1][i]
@@ -23,10 +24,15 @@ def main():
             for j in temp:
                 dict[name][j] = temp[j]
             temp.clear()
+            for j in keys:
+                temp[j] = "Nan"
+    for i in dict:
+        print(len(dict[i]))
+
     file = open("psrcat.json", "w")
     json.dump(dict, file)
     file.close()
-    print(dict["J2339-0533"])
+    # print("P0" in dict["J0007+7303"])
 
 
 if __name__ == '__main__':
